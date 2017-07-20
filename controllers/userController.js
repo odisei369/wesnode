e1a656fdfc02b0e965c8265be5e708ced195d0c3
odisei369 <petrenko.ilia@gmail.com>
2017-07-20 14:58:30 +0200

    add login and registration forms

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+
+exports.loginForm = (req, res) =>{
+    res.render('login', {title: 'Login'});
+
+};
+
+exports.registerForm = (req, res) =>{
+  res.render('register', {title: 'Register'})
+};
+
+exports.validateRegister = (req, res, next) =>
+{
+    req.sanitizeBody('name');
+    req.checkBody('name', 'You must supply name!').notEmpty();
+    req.checkBody('email', 'Email should be valid').isEmail();
+    req.sanitizeBody('email').normalizeEmail({
+        remove_dots: false,
+        remove_extension: false,
+        gmail_remove_subaddress: false
+    });
+    req.checkBody('password', 'Password should not be empty').notEmpty();
+    req.checkBody('password-confirm', 'Password Confirm should not be empty').notEmpty();
+    req.checkBody('password-confirm', 'Password Confirm should match').equals(req.body.password);
+
+    const errors = req.validationErrors();
+    if(errors){
+        req.flash('error', errors.map(err => err.msg));
+        res.render('register', {title: 'Register', body: req.body, flashes: req.flash()});
+        return;
+    }
+    next();
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
-
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 
@@ -29,5 +29,14 @@ router.post('/add/:id',
 router.get('/tags', catchErrors(storeController.getStoreByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoreByTag));
 
+
+router.get('/login', userController.loginForm);
+router.post('/login', userController.loginForm);
+
+router.get('/register', userController.registerForm);
+//validate registration data
+//register the user
+//log them in
+router.post('/register', userController.validateRegister, userController.registerForm);
 
 module.exports = router;
