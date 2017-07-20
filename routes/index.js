c9fa106837d6ced8f341e04c49f27127023af045
odisei369 <petrenko.ilia@gmail.com>
2017-07-20 15:43:57 +0200

    handle registration

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 
@@ -37,6 +38,10 @@ router.get('/register', userController.registerForm);
 //validate registration data
 //register the user
 //log them in
-router.post('/register', userController.validateRegister, userController.registerForm);
+router.post('/register',
+    userController.validateRegister,
+    userController.register,
+    authController.login
+);
 
 module.exports = router;
